fc6f7e2b48a5f8da6ea27feca516aea8eb223713
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-04-14 22:46:47 -0600

    manacher's algorithm added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,60 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include "res.h"
+
+
+std::string Algorithm::manacher(std::string transmissionText){ 
+    int N = transmissionText.size();
+    if (N == 0)
+        return "";
+    N = 2*N + 1; // Position count
+    std::vector<int> L(N, 0); // LPS Length Array
+    L[0] = 0;
+    L[1] = 1;
+    int C = 1; // centerPosition
+    int R = 2; // centerRightPosition
+    int i = 0; // currentRightPosition
+    int iMirror; // currentLeftPosition
+    int maxLPSLength = 0;
+    int maxLPSCenterPosition = 0;
+    int start = -1;
+    int end = -1;
+    int diff = -1;
+
+    for (i = 2; i < N; i++) {
+        // get currentLeftPosition iMirror for currentRightPosition i
+        iMirror = 2*C-i;
+        L[i] = 0;
+        diff = R - i;
+        // If currentRightPosition i is within centerRightPosition R
+        if (diff > 0)
+            L[i] = std::min(L[iMirror], diff);
+
+        // Attempt to expand palindrome centered at currentRightPosition i
+        while (((i + L[i]) < N && (i - L[i]) > 0) && 
+            (((i + L[i] + 1) % 2 == 0) || 
+            (transmissionText[(i + L[i] + 1)/2] == transmissionText[(i - L[i] - 1)/2]))) {
+            L[i]++;
+        }
+
+        if (L[i] > maxLPSLength) { // Track maxLPSLength
+            maxLPSLength = L[i];
+            maxLPSCenterPosition = i;
+        }
+
+        // If palindrome centered at currentRightPosition i 
+        // expand beyond centerRightPosition R,
+        // adjust centerPosition C based on expanded palindrome.
+        if (i + L[i] > R) {
+            C = i;
+            R = i + L[i];
+        }
+    }
+    start = (maxLPSCenterPosition - maxLPSLength)/2;
+    end = start + maxLPSLength - 1;
+    return transmissionText.substr(start, end-start+1);
+}
 
 class KMP_Searcher {
 private:
@@ -75,26 +129,10 @@ public:
 
 int main() {
     KMP_Searcher buscador;
+    Algorithm find;
+
     std::string transmission1 = buscador.readFileIntoString("transmission1.txt");
     std::string transmission2 = buscador.readFileIntoString("transmission2.txt");
-    std::string mcode1 = buscador.readFileIntoString("mcode1.txt");
-
-    std::tuple<bool,int, int> tr1_mc1 = buscador.KMP_Search(transmission1, mcode1);
-    std::tuple<bool, int, int> tr2_mc1 = buscador.KMP_Search(transmission2, mcode1);
-
-    if (std::get<0>(tr1_mc1)) {
-        std::cout << "Mcode1 se encuentra dentro de Transmision1 en la posicion: " 
-                  << std::get<1>(tr1_mc1) << " a " << std::get<2>(tr1_mc1) << std::endl;
-    } else {
-        std::cout << "Mcode1 no se encuentra dentro de Transmision1" << std::endl;
-    }
-
-    if (std::get<0>(tr2_mc1)) {
-        std::cout << "Mcode1 se encuentra dentro de Transmision2 en la posicion: " 
-                  << std::get<1>(tr2_mc1) << " a " << std::get<2>(tr2_mc1) << std::endl;
-    } else {
-        std::cout << "Mcode1 no se encuentra dentro de Transmision2" << std::endl;
-    }
-
-    return 0;
+    
+    std::cout << find.manacher(transmission1);
 }
