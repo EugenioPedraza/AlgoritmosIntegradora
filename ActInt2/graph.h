55c43c200376061bc638f46c2e67956bbad5aa56
Eugenio Raúl Pedraza González <88684940+EugenioPedraza@users.noreply.github.com>
2024-05-13 17:20:53 -0600

    Update graph.h

--- ActInt2/graph.h
+++ ActInt2/graph.h
@@ -1,23 +1,38 @@
-#include <iostream>
+// graph.h
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
 #include <vector>
+#include <iostream>
 #include <limits>
 
+// Estructura para representar una arista
+struct Edge {
+    int src, dest, weight;
+};
 
+// Clase para representar el grafo
 class Graph {
-    private:
+private:
     std::vector<std::vector<int>> adjMatrix;
+    std::vector<Edge> edges;
     int vertexCount;
 
-    public:
+public:
     // Constructor
     Graph(int vertexCount);
 
-    // CRUD methods
+    // Métodos CRUD
     void addEdge(int row, int column, int weight);
-
-    void removeEdge(int row, int column);
-
-    // Print the adjacency matrix
     void printMatrix();
 
+    // Encontrar MST usando Kruskal
+    void kruskalMST();
+
+private:
+    int find(std::vector<int>& parent, int i);
+    void unionSets(std::vector<int>& parent, std::vector<int>& rank, int x, int y);
 };
+
+#endif // GRAPH_H
